9490737f4e5dd308cc45fd3d3863282c2c1849e3
asd45618 <asd45618@naver.com>
2023-08-26 18:50:37 +0900

    codecamp-frontend

---
+++ class/pages/01-02-emotion/index.js
@@ -0,0 +1,11 @@
+import { Email, EmailInput } from "../../styles/emotion";
+
+export default function EmotionPage() {
+  return (
+    <div>
+      <Email>이메일</Email>
+      <EmailInput type="text"></EmailInput>
+      <button>클릭하세요!!</button>
+    </div>
+  );
+}
