9348e9a6adeffa06464249b31b6fb507904ba704
asd45618 <asd45618@naver.com>
2023-09-09 18:29:30 +0900

    react-rest-get

---
+++ class/pages/04-01-rest-get/index.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export default function RestGetPage() {
+  function onClickAsync() {
+    const result = axios.get("https://koreanjson.com/posts/1");
+    console.log(result);
+  }
+
+  //   async function onClickSync() {
+  //     const result = await axios.get("https://koreanjson.com/posts/1");
+  //     console.log(result.data.title);
+  //   }
+
+  const onClickSync = async () => {
+    const result = await axios.get("https://koreanjson.com/posts/1");
+    console.log(result.data.title);
+  };
+
+  return (
+    <div>
+      <button onClick={onClickAsync}>REST-API(비동기) 요청하기</button>
+      <button onClick={onClickSync}>REST-API(동기) 요청하기</button>
+    </div>
+  );
+}
