dbfffcfd553b91e742d7ec320888bc1e57d1e1e6
asd45618 <asd45618@naver.com>
2023-09-09 20:14:16 +0900

    04-quiz-rest-api

---
+++ class_quiz/pages/04-01/index.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export default function RestApiQuiz01() {
+  const onClickRest = async () => {
+    let result = await axios.get("https://koreanjson.com/users");
+    console.log(result);
+  };
+
+  return <button onClick={onClickRest}>REST-API 요청하기</button>;
+}
---
+++ class_quiz/pages/04-02/index.js
@@ -0,0 +1,3 @@
+export default function GraphqlQuiz01() {
+  return <button>GRAPHQL-API 요청하기</button>;
+}
