eedb32bec4e5e229543f71d070ce266e1ae06ea9
asd45618 <asd45618@naver.com>
2023-09-09 20:32:24 +0900

    04-quiz-graphql

--- class_quiz/pages/04-02/index.js
+++ class_quiz/pages/04-02/index.js
@@ -1,3 +1,52 @@
+import { useMutation, gql } from "@apollo/client";
+import { useState } from "react";
+
+const CREATE_BOARD = gql`
+  mutation createBoard($writer: String, $title: String, $contents: String) {
+    createBoard(writer: $writer, title: $title, contents: $contents) {
+      _id
+      number
+      message
+    }
+  }
+`;
+
 export default function GraphqlQuiz01() {
-  return <button>GRAPHQL-API 요청하기</button>;
+  const [writer, setWriter] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  const [createBoard] = useMutation(CREATE_BOARD);
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+  };
+
+  const onClickSubmit = async () => {
+    const result = await createBoard({
+      variables: {
+        writer,
+        title,
+        contents,
+      },
+    });
+    console.log(result);
+  };
+
+  return (
+    <div>
+      작성자: <input type="text" onChange={onChangeWriter} />
+      제목: <input type="text" onChange={onChangeTitle} />
+      내용: <input type="text" onChange={onChangeContents} />
+      <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>
+    </div>
+  );
 }
