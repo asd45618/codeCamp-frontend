928a0f4b1b358bb9a8b0e5a52b3e565ad7a4585e
asd45618 <asd45618@naver.com>
2023-09-11 21:30:41 +0900

    react-dynamic-routing-board-mutation

---
+++ class/pages/05-05-dynamic-routing-board-mutation/index.js
@@ -0,0 +1,42 @@
+import { useMutation, gql } from "@apollo/client";
+import { useRouter } from "next/router";
+
+const myGraphqlSetting = gql`
+  mutation createBoard($writer: String, $title: String, $contents: String) {
+    createBoard(writer: $writer, title: $title, contents: $contents) {
+      _id
+      number
+      message
+    }
+  }
+`;
+
+export default function GraphqlMutationPage() {
+  const router = useRouter();
+
+  const [MyFunction] = useMutation(myGraphqlSetting);
+
+  const onClickSubmit = async () => {
+    try {
+      // try에 있는 내용을 시도하다가 실패하면 다음에 있는 모든 줄들을 무시하고 catch에 있는 내용이 실행
+
+      const result = await MyFunction({
+        variables: {
+          //variables -> $ 역활
+          writer: "훈이",
+          title: "안녕하세요!!",
+          contents: "반갑습니다.",
+        },
+      });
+      console.log(result);
+      console.log(result.data.createBoard.number);
+      router.push(
+        `/05-05-dynamic-routing-board-mutation-moved/${result.data.createBoard.number}`
+      );
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>;
+}
