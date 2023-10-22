6f1dbafd123e1240617161ddcf0bd2e6aaf1d6e2
asd45618 <asd45618@naver.com>
2023-10-22 20:37:41 +0900

    typescript-basic

--- class/pages/_app.tsx
+++ class/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { AppProps } from "next/app";
 
-export default function App({ Component }) {
+export default function App({ Component }: AppProps) {
   const client = new ApolloClient({
     uri: "http://backend-example.codebootcamp.co.kr/graphql",
     cache: new InMemoryCache(), // 컴퓨터의 메모리에 백엔드에서 받아온 데이터를 임시로 저장해 놓기 => 나중에 더 자세히 알아보기
